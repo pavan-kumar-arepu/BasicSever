d749402095ac1834440c14797adb6e87f50620b6
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-05-30 14:13:37 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,39 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/user", (req, res) => {
+  res.send("Hello Pavan! How are you");
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We created your username " + req.body.username);
+});
+
+app.delete("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We deleted your username " + req.body.username);
+});
+
+app.put("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We have updated your username " + req.body.username);
+});
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
+
+exports.app = onRequest(app);
